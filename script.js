37ff39847530326319f9568fd13eb86d55e96fc2
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2023-05-03 09:05:01 +0100

    added a doc for the getComputerChoice function

--- script.js
+++ script.js
@@ -7,6 +7,10 @@
 // console.log(playRound(playerSelection, computerSelection));
 
 function getComputerChoice(){
+    /**
+     * This function randomly selects a single word from the word array
+     * It is the function for the computer playing part of the game
+     */
     const words = ["Rock", "Paper", "Scissors"];
     const randomIndex = Math.floor(Math.random() * words.length);
     const randomWord = words[randomIndex];
